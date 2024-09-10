dbfa98c14608bb9a6b2928b768e82260419cfd44
theycraveken <s12222402@students.duvalschools.org>
2024-09-10 13:21:33 -0400

    starting my code for project

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -0,0 +1,25 @@
+# Awarding Extra Lives, Spencer, Kennedy, v0.0
+
+
+lives = 3 
+score = 500 
+
+# Allow the user to input the score AS AN INTERGER 
+
+# Allow the user to input the score.
+# If score is 1000 or less
+    # Lose a life
+# If score is > 10000 but less than 100001 
+
+    # Give 1 Extra Life 
+# If score is > 100000 
+    #Give 2 Extra Lives 
+
+# Output the score and number of lives to the screen.  
+
+if score <= 1500: 
+      print("You will not continue and will remain with 2 live!\n")
+elif score < 6:
+    print("You're tall enought to ride the roller coaster !\n")
+else: # " oh, no, somethings wrong!"
+    print("Error, height not detected. Do not ride.\n")

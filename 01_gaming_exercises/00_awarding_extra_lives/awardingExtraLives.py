98ea27287865dc2885c1c2ea477a460ba3ef075e
theycraveken <s12222402@students.duvalschools.org>
2024-09-12 13:11:30 -0400

    finished extra lives projects

--- 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -3,13 +3,27 @@
 
 lives = 3 
 score = 500 
+name= "player"
+
+
+
+# print(f"Hello {name}! You scared {score} points.\n")
+
+
+
+# CHARACTER REFERENCE 
+# CURLY BRACES {}
+# ANGLE-BRACKETS<>
+# PARENTHESIS ()
+
 
 # Allow the user to input the score AS AN INTERGER 
 
 # Allow the user to input the score.
-# If score is 1000 or less
+
+# If score is 10000 or less
     # Lose a life
-# If score is > 10000 but less than 100001 
+# Else score is > 10000 but less than 100001 
 
     # Give 1 Extra Life 
 # If score is > 100000 
@@ -17,9 +31,15 @@ score = 500
 
 # Output the score and number of lives to the screen.  
 
-if score <= 1500: 
-      print("You will not continue and will remain with 2 live!\n")
-elif score < 6:
-    print("You're tall enought to ride the roller coaster !\n")
+if score <= 10000: 
+    print("You will not continue and will lose 1 life!\n")
+    #lives = lives - 1 
+    lives -= 1 
+elif score < 100001:
+    print("You're eligile to continue and an extra life will be added!\n") 
+    lives += 2 
 else: # " oh, no, somethings wrong!"
-    print("Error, height not detected. Do not ride.\n")
+    print("Error, score not detected. Do not continue.\n")
+    lives += 2
+
+print(f"Greetings {name}! You have {lives} remaining after scoring {score} points.\n")

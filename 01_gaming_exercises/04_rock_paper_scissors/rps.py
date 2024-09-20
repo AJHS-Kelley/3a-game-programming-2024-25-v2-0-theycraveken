689c85e4c2bf50855d77c4a1a247feb5bf39b953
theycraveken <s12222402@students.duvalschools.org>
2024-09-20 13:13:37 -0400

    catching up the beginning

--- 01_gaming_exercises/04_rock_paper_scissors/rps.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.py
@@ -1,25 +1,29 @@
-Rock, Paper, Scissors, Kennedy Spencer v0.1
+# Rock, Paper, Scissors, Kennedy Spencer v0.1
 
+# MODULE IMPORTS
+import random 
 
+# DATA STRUCTURES -- PLAYER
+playerScore = 0
+playerName = "Kennedy" 
+playerChoice = None 
 
+# DATA STRUCTURES -- CPU 
+cpuScore = 0
+cpuChoice = None
 
+# PLAYER NAME INOUT 
+playerName = input("Please type your name and press enter.\n")
+print(f"Hello{playerName}!\n")
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
 
+# .lower() can turn ALL input into lowercase.
+# .upper() can turn ALL input into UPPERCASE.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if isCorrect == "yes": 
+    print(f"ok {playerName}, let's play Rock, Paper, Scissors!\n")
+else:
+    playerName = input("Please type your name and press enter.\n")
 
 
 
@@ -52,8 +56,46 @@ putting a # in front of 15 different lines.
 while playerScore < 5 and cpuScore < 5: 
     print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n") 
     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+    if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors"
+        playerChoice = input("Please enter rock, paper, or scissors and press eneter.\n").lower() 
+         if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
+        print("You are not following directions. Please try again.\n")
+        exit()
+    print(f"You have chosen {playerChoice}.\n")
+else: 
+    print(f"You have chosen {playerChoice}.\n")
+    
+    
+    
+    
+    
+    # let cpu select choice at random 
+    cpuChoice = random .randint(0, 2)    #randomly select 0, 1, 0r 2. 
+    if cpuChoice == 0:
+        cpuChoice = "rock"
+    elif cpuChoice == 1: 
+        cpuChoice = "scissors"
+    elif cpuChoice == 2: 
+        cpuChoice = "paper" 
+    else: 
+        print("Unable to determine CPU choice.\nPlease restart.\n")
+        exit()
+    #print(f"CPU Choice: {cpuChoice}") 
 
-    # let cpu select choice at random
+
     # compare player choice to cpu choice 
+    if playerChoice == "rock" and cpuChoice == "paper"
+        pass
+        #CPU WINS 
+    elif playerChoice == "rock" and cpuChoice == "scissors"
+        print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+        print("You win a ponit.\n")
+        playerScore += 1  
+        # PLAYER WINS 
+
+    elif playerChoice == "rock"
+
+
+
     # print the results to the screen 
     # award point to winner and output results.

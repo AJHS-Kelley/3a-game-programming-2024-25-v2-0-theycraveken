fe808153a43e5d6b7b228703d96982d8c26e3220
theycraveken <s12222402@students.duvalschools.org>
2024-10-16 13:18:24 -0400

    did returns and determined str

--- 01_gaming_exercises/04_rock_paper_scissors/rps3.0.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps3.0.py
@@ -13,46 +13,43 @@ cpuScore = 0
 cpuChoice = None
 
 # PLAYER NAME INPUT 
-def playerName(): # Function Singnature -- name of function, (arguments if any)
+def playerName() -> str: # Function Singnature -- name of function, (arguments if any)
+    """"Gets the name from the player and returns it."""
+    # The line above is a DOCSTRING, it gives a breif example of what the function does.
     playerName = input("Please type your name and press enter.\n")
     print(f"Hello{playerName}!\n")
     isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
-
-# .lower() can turn ALL input into lowercase.
-# .upper() can turn ALL input into UPPERCASE.
 
     if isCorrect == "yes": 
         print(f"ok {playerName}, let's play Rock, Paper, Scissors!\n")
     else:
         playerName = input("Please type your name and press enter.\n")
+    return playerName 
 
 # CALLING THE FUNCTION
-playerName() 
+playerName = playerName()
 
-# THE RULES using MULTI-LINE STRINGS
-print(""""
-Welcome to Rock, Paper, Scissors Robot!
-It's Time to play Rock, Paper, Scissors!
+# THE RULES using MULTI-LINE STRINGS 
+def rules() -> None: 
+    """This function prints the rules for rock, paper, scissors."""
+    print(f"""
+    Welcome to Rock, Paper, Scissors Robot!
+    It's Time to play Rock, Paper, Scissors!
 
-You will play against the CPU. The first to score 3 points wins.
-You will select from ROCK, PAPER, or SCISSORS.
-The cpu will select ROCK, PAPER or SCISSORS at randoms. 
+    You will play against the CPU. The first to score 3 points wins.
+    You will select from ROCK, PAPER, or SCISSORS.
+    The cpu will select ROCK, PAPER or SCISSORS at randoms. 
       
-1. ROCK BEATS SCISSORS!
-2. SCISSORS BEATS PAPER!
-3. PAPER BEATS ROCK!
-""") 
+    1. ROCK BEATS SCISSORS!
+    2. SCISSORS BEATS PAPER!
+    3. PAPER BEATS ROCK!
+    """) 
+# Does another part of this program need to access this information?
+# IF YES, YOU MUST HAVE A return STATEMENT
+# IF NO, A return STATEMENT IS NOT REQUIRED
 
-#Multi-LINE STRINGS CAN BE USED AS BIG COMMENTS
-""" 
-Anything in between the sets of a double quotes is just ignored.
-If you need to write large comments, it's easier to use multi-line strings than 
-putting a # in front of 15 different lines.
-""" 
-
-#MAIN GAME LOOP 
-while playerScore < 5 and cpuScore < 5: 
-    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n") 
+def playerChoice() -> str:
+    """Allows the CPU to choose rock, paper, scissors."""
     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
     if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
         playerChoice = input("Please enter rock, paper, or scissors and press eneter.\n").lower() 
@@ -60,6 +57,14 @@ while playerScore < 5 and cpuScore < 5:
             print("You are not following directions. Please try again.\n")
             exit()
     print(f"You have chosen {playerChoice}.\n")
+    return playerChoice
+
+
+
+#MAIN GAME LOOP 
+while playerScore < 5 and cpuScore < 5: 
+    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n") 
+    
    # else: 
         #print(f"You have chosen {playerChoice}.\n")
     

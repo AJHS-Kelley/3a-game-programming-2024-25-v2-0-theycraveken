5442977d2e9a94a52d5c47f052dca3514b4a4ee5
theycraveken <s12222402@students.duvalschools.org>
2024-11-07 13:18:50 -0500

    trying to change maps

--- 01_gaming_exercises/05_dragonRealm.py
+++ 01_gaming_exercises/05_dragonRealm.py
@@ -1,4 +1,4 @@
-# Dragon Realm, <STUDENT_NAME>, v0.0
+# Dragon Realm, <Kennedy Spencer>, v0.1
 # Based on https://inventwithpython.com/chapter6.html by Al Sweigart
 
 import random
@@ -8,19 +8,22 @@ def displayIntro():
 
     print('You are on the moon with the aliens. In front of you,')
     print('you see two portal maps. In one map, you go to the army where its fun')
-    print('and the people will treasure you with love. The other map')
-    print('is full of slaves, and will eat you on sight.')
+    print('and the people will treasure you with love. The other map Ancient')
+    print('is full of slaves, and will torcher and kill you.')
     print()
 
 def chooseMap():
     map = ''
-    while map != '1' and map != '2':
+    while map1 != '1' and map2 != '2':
         print('Which map will you go choose? (1 or 2)')
         map = input()
     return map
 
-def checkMap(chosenCave):
-    print('You approach the cave...')
+map1 = 'Army' 
+map2 = 'Ancient'
+
+def checkMap(Army):
+    print('You approach the Army...')
     time.sleep(2)
     print('It is dark and spooky...')
     time.sleep(2)
@@ -30,11 +33,17 @@ def checkMap(chosenCave):
 
     friendlyMap = random.randint(1, 2)
 
-    if chosenMap == str(friendlyMap):
+    if Army == str(friendlyMap):
         print('Gives you his treasure!')
 
     else:
         print('Gobbles you down in one bite!')
+
+def checkMap(Ancient):
+    print('You enter the Ancient...')
+    time.sleep(2)
+
+
 
 
 
@@ -43,6 +52,6 @@ playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
     displayIntro()
     mapNumber = chooseMap()
-    checkMap(caveMap)
+    checkMap(mapNumber)
     print('Do you want to play again? (yes or no)')
     playAgain = input()

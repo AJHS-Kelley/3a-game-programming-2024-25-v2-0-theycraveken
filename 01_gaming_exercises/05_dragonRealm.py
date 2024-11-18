54e92cd92a11c98e77be7de1bbeabb41ee6d602e
theycraveken <s12222402@students.duvalschools.org>
2024-11-18 13:21:22 -0500

    fixing shit

--- 01_gaming_exercises/05_dragonRealm.py
+++ 01_gaming_exercises/05_dragonRealm.py
@@ -7,18 +7,18 @@ import datetime
 
 # SAVONG DATA TO A FILE 
 # STEP 1 -- creating the file name to use.
-logFileName = "dragonRealmLog" + str(time.time()) + ".txt"
-#logFileName = "dragonRealmLog.txt"
+
+logFileName = 'dragonRealmLog.txt'
 # Example: dragonRealmLog1132AM.txt 
 
 # Step 2 -- Create/ open the file to save the data 
-saveData = open(logFileName, "a")
+saveData = open(logFileName, 'a')
 # FILE MODES
-# "x" Creates file, if file exist, exit with erroe message.
-# "w" Creates file, if file exist, erase and overwrite file contents.
-# "a" Creates file, if file exists, append data to the file.
+# 'x' Creates file, if file exist, exit with erroe message.
+# 'w' Creates file, if file exist, erase and overwrite file contents.
+# 'a' Creates file, if file exists, append data to the file.
 
-saveData.write("GAME STARTED" + " " + str(datetime.datetime.now()) + "\n")
+saveData.write('GAME STARTED' + ' ' + str(datetime.datetime.now()) + '\n')
 
 
 
@@ -38,47 +38,42 @@ def displayIntro():
 
 def chooseMap():
     map = ''
-    while map1 != '1' and map2 != '2':
+    while map != '1' and map != '2':
         print('Which map will you go choose? (1 or 2)')
         map = input()
-        if map1 == '1':
-            print("Excellent choice my friend!....")
-            time.sleep(2)
-            print("You're entering the Army......")
-            time.sleep(2)
-        elif map2 == '2':
-            print("Interesting choice my friend.....")
-            time.sleep(2)
-            print("I see you like to suffer...")
-            time.sleep(2)
-            print("lets see if you can escape!")
-        else: 
-            input("Please enter a valid number in order to continue.\n")
+        print(' If choose ')
     return map
+    
 
-map1 = 'Army' 
-map2 = 'Ancient'
 
-def checkMap(Army):
-    print('You approach the Army...')
-    time.sleep(2)
-    print('It is dark and spooky...')
-    time.sleep(2)
-    print('A large dragon jumps out in front of you! He opens his jaws and...')
-    print()
-    time.sleep(2)
+
+def checkMap(map):
+    if map == '1':
+        if map == '1':
+            saveData.write('Player choose the Army')
+            print('Excellent choice my friend!....')
+            time.sleep(2)
+            print('You\'re entering the Army......')
+            time.sleep(2)
+        elif map == '2':
+            print('Interesting choice my friend.....')
+            time.sleep(2)
+            print('I see you like to suffer...')
+            time.sleep(2)
+            print('lets see if you can escape!')
+        else: 
+            input('Please enter a valid number in order to continue.\n')
+    return map
 
     friendlyMap = random.randint(1, 2)
 
-    if Army == str(friendlyMap):
+    if map == str(friendlyMap):
         print('Gives you his treasure!')
 
     else:
         print('Gobbles you down in one bite!')
 
-def checkMap(Ancient):
-    print('You enter the Ancient...')
-    time.sleep(2)
+
 
 
 
@@ -97,5 +92,6 @@ while playAgain == 'yes' or playAgain == 'y':
 
 
 
-    # CLOSE THE FILE 
+    # CLOSE THE FILE
+saveData.write('END OF GAME LOG\n\n') 
 saveData.close()

596b7773e8668579741fad21c2ba65a570e1d8ae
theycraveken <s12222402@students.duvalschools.org>
2024-11-20 12:11:49 -0500

    items fixed

--- 01_gaming_exercises/05_dragonRealm/OLD_05_dragonRealm.py
+++ 01_gaming_exercises/05_dragonRealm/OLD_05_dragonRealm.py
@@ -21,6 +21,13 @@ saveData = open(logFileName, 'a')
 saveData.write('GAME STARTED' + ' ' + str(datetime.datetime.now()) + '\n')
 
 
+
+
+
+
+
+
+
 def displayIntro():
 
     print('You are on the moon with the aliens. In front of you,')
@@ -31,41 +38,40 @@ def displayIntro():
 
 def chooseMap():
     map = ''
-    while map != '1' and map != '2':
-        print('Choose if you dare....\n ')
-        print('{1} You will teleport to the Army!\n{2} You will teleport to the Ancient world\n')
-        print('Which map will you go choose? (1 or 2)')
-        print('Insert the number of your desired map and press PLAY.\n')
-        map = input()
-        print(map)
+    print('Choose if you dare....\n ')
+    print('{1} You will teleport to the Army!\n{2} You will teleport to the Ancient world\n')
+    print('Which map will you go choose? (1 or 2)')
+    print('Insert the number of your desired map and press PLAY.\n')
+    map = input()
     return map
     
 
 
 def checkMap(map):
-    
-    if map == '1':
-        saveData.write('Player choose the Army')
-        print('Excellent choice my friend!....')
-        time.sleep(2)
-        print('You\'re entering the Army......')
-        time.sleep(2)
-    elif map == '2':
-        print('Interesting choice my friend.....')
-        time.sleep(2)
-        print('I see you like to suffer...')
-        time.sleep(2)
-        print('lets see if you can escape!')
-    else: 
+   while map != '1' and map != '2':
+        if map == '1':
+            saveData.write('Player choose the Army')
+            print('Excellent choice my friend!....')
+            time.sleep(2)
+            print('You\'re entering the Army......')
+            time.sleep(2)
+        elif map == '2':
+            print('Interesting choice my friend.....')
+            time.sleep(2)
+            print('I see you like to suffer...')
+            time.sleep(2)
+            print('lets see if you can escape!')
+        else: 
             input('Please enter a valid number in order to continue.\n')
-    return map
+        return map
 
-def checkitems():
-    items = ""
-    while items != "11" and items != "21":
-        print("You may need some items to get you through your journey...")
-        print("{11} For the Army you can choose a ")
+friendlyMap = random.randint(1, 2)
 
+if map == str(friendlyMap):
+        print('Gives you his treasure!')
+
+else:
+        print('Gobbles you down in one bite!')
 
 
 
@@ -78,7 +84,6 @@ playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
     displayIntro()
     mapNumber = chooseMap()
-    print(mapNumber)
     checkMap(mapNumber)
     print('Do you want to play again? (yes or no)')
     playAgain = input()
